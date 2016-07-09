b02ea5d6d20fddbfadae593ee90f09c99f8fd552
charlie41641 <marincasdenis@yahoo.com>
2016-07-09 11:49:18 +0300

    on click show buttons

--- scripts/js.js
+++ scripts/js.js
@@ -2,7 +2,13 @@
 $(document).ready(function(){
 	$('#button').on('click',function(){
 		$('body').toggleClass('activated');
+
 	});
+	$('.reveal').click(function(){
+		$('.img-container').show();
+		});
+ 	
 });
+	
 
 	
